78a348008eded0a2997cefdcb5eca475a44a3827
Laura Herrera <est.lauraa.herrerae@unimilitar.edu.co>
2026-03-26 10:46:09 -0500

    me marca error ya que no estoy enlazando los nodos

--- mavenproject1/src/main/java/Lista.java
+++ mavenproject1/src/main/java/Lista.java
@@ -1,5 +1,12 @@
 
 public class Lista {
 
-        
+        Nodo primerNodo;
+    public Lista(){
+            this.primerNodo=null;
+        }
+    public void insertar(int dato){
+        Nodo nuevoNodo=new Nodo(dato;
+        this primerNodo=nuevoNodo;
+    }    
 }

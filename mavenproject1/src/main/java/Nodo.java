db53ca5dd8696ff35eca211a3f7bebde6b667438
Laura Herrera <est.lauraa.herrerae@unimilitar.edu.co>
2026-03-26 10:34:05 -0500

    se crea la clase Nodo y Lista, en la clase Nodo se crea el atributo dato y referencia suiguienteNodo, tuve un error al  hacerlo ya que estaba colocando una variable que puse en la clase Taller21260326, ya esta corregido

---
+++ mavenproject1/src/main/java/Nodo.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Admin
+ */
+public class Nodo {
+    int dato;
+    Nodo siguienteNodo;
+    
+    public Nodo(int dato){
+        this.dato=dato;
+        this.siguienteNodo=null;
+        
+    }
+}
--- mavenproject1/src/main/java/Taller21260326.java
+++ mavenproject1/src/main/java/Taller21260326.java
@@ -2,8 +2,9 @@ import java.util.Scanner;
 public class Taller21260326 {
 
     public static void main(String[] args) {
+        
         Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
+        int n =sc.nextInt();
        
        
     }

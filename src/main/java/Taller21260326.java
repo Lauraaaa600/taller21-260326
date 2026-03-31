1b4acfd3b20e77a72b23f889feefd229fc005526
Laura Alexandra Herrera Espitia <est.lauraa.herrerae@unimilitar.edu.co>
2026-03-31 18:25:52 -0500

    el error fue que en el ciclo for debe estar ajustado a n-1,ademas de que le coloque un = q no era, ya esta corregido

--- src/main/java/Taller21260326.java
+++ src/main/java/Taller21260326.java
@@ -11,7 +11,7 @@ public class Taller21260326 {
        
      Lista lista=new Lista();
      
-     for(int i=0; i=<n;i++){
+     for(int i=0; i<n-1;i++){
          //error
          int numero=sc.nextInt();
          lista.insertar(numero);

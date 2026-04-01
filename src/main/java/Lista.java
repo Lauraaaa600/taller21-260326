937b077d68270655f2ecc5640ab0687c2c810b8e
Laura Alexandra Herrera Espitia <est.lauraa.herrerae@unimilitar.edu.co>
2026-03-31 19:43:45 -0500

    Al momento de darle run el codigo no quedo como queria ya que quedaba desordenado por lo que agrege un nuevo metodo en la clase Lista en este caso fue MostrarOrdenado

--- src/main/java/Lista.java
+++ src/main/java/Lista.java
@@ -7,9 +7,15 @@ public class Lista {
         }
     public void insertar(int dato){
         Nodo nuevoNodo=new Nodo(dato);
-        nuevoNodo.siguienteNodo=this.primerNodo;
-        this.primerNodo=nuevoNodo;
+        nuevoNodo.siguienteNodo=primerNodo;
+        
     }    
+    public void mostrarOrdenado(Nodo Nodo){
+        if(primerNodo==null)return;
+        mostrarOrdenado(primerNodo.siguienteNodo);
+        System.out.print(primerNodo.dato);
+        
+    }
     public int calcularFaltante(int n){
         int suma = n*(n+1)/2;
         for(Nodo actual = primerNodo;actual!=null;actual=actual.siguienteNodo){

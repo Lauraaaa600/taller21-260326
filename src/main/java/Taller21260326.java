bd8f6b2428e470215a9791bfbe0dba5d2afc7363
Laura Alexandra Herrera Espitia <est.lauraa.herrerae@unimilitar.edu.co>
2026-03-31 19:58:47 -0500

    use la libreria Random para generar valores aleatorios

--- src/main/java/Taller21260326.java
+++ src/main/java/Taller21260326.java
@@ -1,9 +1,12 @@
 import java.util.Scanner;
+import java.util.Random;
+
 public class Taller21260326 {
 
     public static void main(String[] args) {
         
         Scanner sc=new Scanner(System.in);
+        Random rand = new Random ();
         
         System.out.println("Ingrese un numero: ");
         int n =sc.nextInt();
